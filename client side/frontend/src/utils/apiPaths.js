521d5d74940fa246a067c37b4bcaabf88a62b8ff
20-BAHHOUS <bahhossihem@gmail.com>
2025-04-08 00:11:44 +0200

    chore: Update API paths in frontend code

--- client side/frontend/src/utils/apiPaths.js
+++ client side/frontend/src/utils/apiPaths.js
@@ -1,27 +1,27 @@
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 // utils/apiPaths.js
 const API_PATHS = {
   AUTH: {
-    LOGIN: `${BASE_URL}/api/v1/auth/login`,
-    REGISTER: `${BASE_URL}/api/v1/auth/register`,
-    GET_USER_INFO: `${BASE_URL}/api/v1/auth/getuser`,
+    LOGIN: `/api/v1/auth/login`,
+    REGISTER: `/api/v1/auth/register`,
+    GET_USER_INFO: `/api/v1/auth/getuser`,
   },
   HOME: {
-    GET_DATA: `${BASE_URL}/api/v1/home`,
+    GET_DATA: `/api/v1/home`,
   },
   ANNONCE: {
-    ADD_ANNONCE: `${BASE_URL}/api/v1/annonce/addannonce`,
-    GET_ALL_ANNONCES: `${BASE_URL}/api/v1/annonce/getallannonces`,
-    DELET_ANNONCE: (annonceId) => `${BASE_URL}/api/v1/annonce/deleteannonce/${annonceId}`,
-    UPDATE_ANNONCE: `${BASE_URL}/api/v1/annonce/updateannonce/:id`,
-    GET_ANNONCE: `${BASE_URL}/api/v1/annonce/getannonce/:id`,
-    SEARCH_ANNONCE: `${BASE_URL}/api/v1/annonce/searchannonces/:query`,
-    GET_ANNONCE_BY_CATEGORY: `${BASE_URL}api/v1/annonce/getannoncesbycategory`,
-    GET_ANNONCE_BY_TYPE: `${BASE_URL}api/v1/annonce/getannoncesbytype`,
+    ADD_ANNONCE: `/api/v1/annonce/addannonce`,
+    GET_ALL_ANNONCES: `/api/v1/annonce/getallannonces`,
+    DELET_ANNONCE: (annonceId) => `/api/v1/annonce/deleteannonce/${annonceId}`,
+    UPDATE_ANNONCE: `/api/v1/annonce/updateannonce/:id`,
+    GET_ANNONCE: `/api/v1/annonce/getannonce/:id`,
+    SEARCH_ANNONCE: `/api/v1/annonce/searchannonces/:query`,
+    GET_ANNONCE_BY_CATEGORY: `api/v1/annonce/getannoncesbycategory`,
+    GET_ANNONCE_BY_TYPE: `api/v1/annonce/getannoncesbytype`,
     
   },
   IMAGE: {
-    UPLOAD_IMAGE: `${BASE_URL}/api/v1/image/upload`,
+    UPLOAD_IMAGE: `/api/v1/image/upload`,
   },
 
 };
